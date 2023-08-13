29a6a5a1490ce101a07010496e2e6ffde9aa9f2a
Антон Котов <achto32@gmail.com>
2023-08-13 03:08:50 +0300

    second reviw

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = bool(os.getenv('DEBUG'))
 
-ALLOWED_HOSTS = ['158.160.24.76', '127.0.0.1', 'localhost', 'myfirstproject.ddns.net']
+ALLOWED_HOSTS = ['158.160.77.126', '127.0.0.1', 'localhost', 'myfirstproject.ddns.net']
 
 
 INSTALLED_APPS = [
